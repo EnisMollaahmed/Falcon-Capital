1148446fadb68a05b744591ddaaf8f35f8df576f
EnisMollaahmed <enis_super02@outlook.com>
2024-02-03 12:23:37 +0200

    balance_and_statistics is ready

--- balance_and_statistics/balance_and_statistics.js
+++ balance_and_statistics/balance_and_statistics.js
@@ -7,7 +7,10 @@ const totalIncomeVisible=document.querySelector(".income .amount");
 const totalIncomeCurrency = document.querySelector(".income .curr");
 const revUsername=document.querySelector(".right-border .username");
 const spentUsername=document.querySelector(".left-border .username");
-
+const incToDateAmount = document.querySelector(".inc .money");
+const incToDateCurr = document.querySelector(".inc .curr");
+const expToDateAmount = document.querySelector(".spend .money");
+const expToDateCurr = document.querySelector(".spend .curr");
 
 function sumIncome(){
     let sum = 0;
@@ -55,8 +58,16 @@ function findTransferValues(targetUsename, transactDirection){
     if(transactDirection === "income-transactions"){
         const userParticipated = actualUser["income-transactions"].filter(element=>element.senderUsername === targetUsename);
         const moneyOfTransfers = userParticipated.map(element=>element.amount);
-        moneyOfTransfers.reduce
+        const sum = moneyOfTransfers.reduce((accumulator, currentValue)=> accumulator + currentValue, 0);
+        return sum;
     }
+    else if(transactDirection === "outcome-transactions"){
+        const userParticipated = actualUser["outcome-transactions"].filter(element=>element.recieverUsername === targetUsename);
+        const moneyOfTransfers = userParticipated.map(element=>element.amount);
+        const sum = moneyOfTransfers.reduce((accumulator, currentValue)=> accumulator + currentValue, 0);
+        return sum;
+    }
+    return 0;
 }
 
 const showInfo = ()=>{
@@ -68,7 +79,16 @@ const showInfo = ()=>{
     totalSpendingCurrency.textContent=actualUser.currency;
     totalIncomeVisible.textContent=income;
     totalIncomeCurrency.textContent=actualUser.currency;
-
+    const revenueUsername = findRevUsername();
+    const expensiveUsername = findExpensiveUsername();
+    revUsername.textContent=revenueUsername;
+    spentUsername.textContent=expensiveUsername;
+    const specificIncome = findTransferValues(revenueUsername, "income-transactions");
+    const specificExpenses = findTransferValues(expensiveUsername, "outcome-transactions");
+    incToDateAmount.textContent=specificIncome;
+    incToDateCurr.textContent=actualUser.currency;
+    expToDateAmount.textContent=specificExpenses;
+    expToDateCurr.textContent=actualUser.currency;
 };
 
 window.addEventListener("load", showInfo);
